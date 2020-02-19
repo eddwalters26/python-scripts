b686a339ead730c13a626c3c650cb184eb8cc6b9
eddwalters26 <61195852+eddwalters26@users.noreply.github.com>
2020-02-19 17:04:01 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,38 @@
-def display_board():
+def generateBoard(boardSize):
+    board = []
+    x = 0
+    while x < boardSize:
+        board.append([0] * boardSize)
+        x += 1
+    return board
+
+def generateWinConditions(boardSize):
+    wins = []
+    #row wins & column wins
+    for row in range(boardSize):
+        rowWin = []
+        colWin = []
+        for col in range(boardSize):
+            rowWin.append([row, col])
+            colWin.append([col, row])
+        wins.append(rowWin)
+        wins.append(colWin)
+    #diagonal wins
+    row = 0
+    col = boardSize - 1
+    diagRight = []
+    diagLeft = []
+    while row < boardSize:
+        diagRight.append([row, row])
+        diagLeft.append([row, col])
+        row += 1
+        col -= 1
+    wins.append(diagRight)
+    wins.append(diagLeft)
+    return wins
+
+def display_board(board):
+    boardSize = len(board)
     x = 0
     print("\n")
     for row in board:
@@ -11,14 +45,16 @@ def display_board():
             else:
                 txt = " O "
 
-            if cellCount < 2:
+            if cellCount < boardSize - 1:
                 txt += "|"
             cellCount += 1
             print(txt, end="")
-        if x < 2:
-            print("\n", "-" * 10)
+        if x < boardSize - 1:
+            print("\n", end="")
+            txt = ("---+" * boardSize)[:-1]
+            print(txt)
         x += 1
-    print("")
+    print("\n")
 
 def getPlayer(xPlayerTurn):
     if xPlayerTurn == True:
@@ -27,7 +63,7 @@ def getPlayer(xPlayerTurn):
         player = "O"
     return player
         
-def playerTurn(xTurn):
+def playerTurn(xPlayerTurn):
     player = getPlayer(xPlayerTurn)
     txt = "Your move {}"
     print(txt.format(player))
@@ -38,19 +74,17 @@ def playerTurn(xTurn):
     else:
         return True, row, col
 
-def playerMove(row, col, xTurn):
+def playerMove(row, col, xPlayerTurn):
     try:
         iRow = int(row) - 1
         iCol = int(col) - 1
     except:
         print("Please enter a integer value")
         return False
-    
-    if xTurn == True:
-        player = "X"
+    player = getPlayer(xPlayerTurn)
+    if xPlayerTurn == True:
         playerValue = 1
     else:
-        player = "O"
         playerValue = -1
 
     try:
@@ -66,19 +100,19 @@ def playerMove(row, col, xTurn):
         print(invalidMoveTxt.format(player))
         return False
 
-def checkWin():
+def checkWin(board, winConditions):
     for win in winConditions:
        total = 0
        for cell in win:
             row = cell[0]
             col = cell[1]
             total += board[row][col]
-       if abs(total) == 3:
+       if abs(total) == len(board):
            return True
     
     return False
 
-def checkBoardFull():
+def checkBoardFull(board):
     moveAvailable = False
     for row in board:
         for cell in row:
@@ -86,49 +120,49 @@ def checkBoardFull():
                 moveAvailable = True
     return moveAvailable
 
+def gameFunction(board, winConditions):
+    xPlayerTurn = False
+    bGameOver = False
+    bMoveAvailable = True
 
-board = [[0, 0, 0],
-         [0, 0, 0],
-         [0, 0, 0]]
-
-winConditions = [[[0,0], [0,1], [0,2]],
-                     [[1,0], [1,1], [1,2]],
-                     [[2,0], [2,1], [2,2]],
-                     [[0,0], [1,0], [2,0]],
-                     [[0,1], [1,1], [2,1]],
-                     [[0,2], [1,2], [2,2]],
-                     [[0,0], [1,1], [2,2]],
-                     [[0,2], [1,1], [2,0]]]
-
-xPlayerTurn = False
-bGameOver = False
-bMoveAvailable = True
-
-while bGameOver == False and bMoveAvailable == True:
-    validMove = False
-    validMoveInput = False
-    display_board()
-
-    while validMove == False:
-        while validMoveInput == False:
-            validMoveInput, row, col = playerTurn(xPlayerTurn)
+    while bGameOver == False and bMoveAvailable == True:
+        validMove = False
         validMoveInput = False
-        validMove = playerMove(row, col, xPlayerTurn)
+        display_board(board)
 
-    bGameOver = checkWin()
+        while validMove == False:
+            while validMoveInput == False:
+                validMoveInput, row, col = playerTurn(xPlayerTurn)
+            validMoveInput = False
+            validMove = playerMove(row, col, xPlayerTurn)
 
-    if bGameOver == True:
-        player = getPlayer(xPlayerTurn)
-        txt = "Congratulations player {} you have won"
-        print(txt.format(player))
+        bGameOver = checkWin(board, winConditions)
+
+        if bGameOver == True:
+            player = getPlayer(xPlayerTurn)
+            txt = "Congratulations player {} you have won"
+            print(txt.format(player))
+            break
+
+        bMoveAvailable = checkBoardFull(board)
+
+        if bMoveAvailable == False:
+            txt = "No more moves the game is tied"
+            print(txt)
+            break
+        xPlayerTurn = not(xPlayerTurn)
+
+    display_board(board)
+
+bPlayAgain = True
+while bPlayAgain == True:
+    boardSize = 4
+    board = generateBoard(boardSize)
+    winConditions = generateWinConditions(boardSize)
+
+    gameFunction(board, winConditions)
+    sPlayAgain = input("Do you want to play again (y/n)?")
+    print(sPlayAgain.lower())
+    if sPlayAgain.lower() == "n":
+        bPlayAgain == False
         break
-
-    bMoveAvailable = checkBoardFull()
-
-    if bMoveAvailable == False:
-        txt = "No more moves the game is tied"
-        print(txt)
-        break
-    xPlayerTurn = not(xPlayerTurn)
-
-display_board()
